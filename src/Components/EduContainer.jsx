a712606820b0cd96be1ca5c5ad4093325a6ccd9d
Samson <samsonsurin22@gmail.com>
2024-05-01 20:40:23 +0530

    fixed educational Page rendering

--- src/Components/EduContainer.jsx
+++ src/Components/EduContainer.jsx
@@ -3,12 +3,10 @@ import React from "react";
 export function EduContainer() {
     return (
     <>
-        <div id="scroll-target" className="absolute w-10/12 max-w-3xl h-80vh left-4 bg-opacity-25 xl:mt-20 flex flex-col items-start justify-center pt-20 pl-8 z-10">
-            <h1 className="m-0 text-black font-extrabold mt-0 xl:mt-0 text-3xl md:text-5xl w-full mb-8">Explore Educational Content</h1>
+        <div id="scroll-target" className="w-10/12 max-w-3xl h-80vh left-4 bg-opacity-25 xl:mt-20 flex flex-col items-start justify-center md:pt-15 xl:pt-20 pl-8">
+            <h1 className="m-0 text-black font-extrabold text-3xl md:text-2xl xl:text-5xl w-full mb-8">Explore Educational Content</h1>
             
             <h3 className="text-black font-semibold mt-4 mb-3 text-xl md:text-2xl">Our Planets</h3>
-
-
         
         </div>
         
