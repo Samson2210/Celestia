e627ef73b9359b25226f871c58002d2a8667a85c
Samson <samsonsurin22@gmail.com>
2024-05-02 10:37:05 +0530

    updated links and updsted edu

--- src/Components/Visit.jsx
+++ src/Components/Visit.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React from 'react';
+import solarSystemImage from '../assets/Edu_Images/ss.jpg';
+import earthImage from '../assets/Edu_Images/earth.gif';
+import moonImage from '../assets/Edu_Images/moon.gif';
+import marsImage from '../assets/Edu_Images/mars.gif';
+
 function SolarSystem() {
   return (
     <div className="planet bg-white rounded-lg overflow-hidden shadow-md">
-    <img src="src/assets/Edu_Images/ss.gif" alt="Solar System" className="w-full h-auto object-cover" />
-    <div className="planet-action flex items-center justify-between p-4">
-      <h3 className="text-lg font-semibold">Solar System</h3>
-      <button className="pic bg-black text-white px-4 py-2 rounded-lg">Visit</button>
+      <img src={solarSystemImage} alt="Solar System" className="w-full  h-1/2 md:h-auto object-cover" />
+      <div className="planet-action flex items-center justify-between p-4">
+        <h3 className="text-lg font-semibold">Solar System</h3>
+        <button className="pic bg-black text-white px-4 py-2 rounded-lg">Visit</button>
+      </div>
     </div>
-  </div>
   );
 }
 
-function Planet({ name, image }) {
+function Planet({ name, image, link }) {
   return (
-    <div className="planet w-64 h-64 border border-gray-300 shadow-md rounded-md m-8">
+    <div className="planet w-full md:w-64 h-64 border border-gray-300 shadow-md rounded-md m-8">
       <img src={image} alt={`${name} Image`} className="w-full h-3/4 object-cover rounded-t-md" />
       <div className="planet-action flex justify-between items-center p-4">
-        <h3>{name}</h3>
-        <button className="pic px-4 py-2 bg-black text-white text-sm font-bold rounded-md">Visit</button>
+        <h3 className='font-semibold'>{name}</h3>
+        <button className="pic px-4 py-2 bg-black text-white text-sm font-bold rounded-md"><a href={link} target='_blank'>Visit</a></button>
       </div>
     </div>
   );
@@ -25,15 +30,15 @@ function Planet({ name, image }) {
 
 function Planets() {
   const planets = [
-    { name: 'Earth', image: 'src/assets/Edu_Images/earth.gif' },
-    { name: 'Moon', image: 'src/assets/Edu_Images/moon.gif' },
-    { name: 'Mars', image: 'src/assets/Edu_Images/mars.gif' }
+    { name: 'Earth', image: earthImage ,link:'https://celestia-earth.onrender.com'},
+    { name: 'Moon', image: moonImage,link:'https://celestia-moon.onrender.com'  },
+    { name: 'Mars', image: marsImage, link:'https://celestia-mars.onrender.com' }
   ];
 
   return (
-    <div className="planets flex justify-center h-full py-10 px-16">
+    <div className="planets flex flex-wrap justify-center h-full py-10 px-4 md:px-16">
       {planets.map((planet, index) => (
-        <Planet key={index} name={planet.name} image={planet.image} />
+        <Planet key={index} name={planet.name} image={planet.image} link={planet.link} />
       ))}
     </div>
   );
@@ -42,10 +47,10 @@ function Planets() {
 const Visit = () => {
   return (
     <>
-    <SolarSystem />
-    <Planets />
-  </>
-  )
+      <SolarSystem />
+      <Planets />
+    </>
+  );
 }
 
-export default Visit
+export default Visit;
